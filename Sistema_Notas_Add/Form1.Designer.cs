84eb35b819a8bd29bb4fbeaeef6252a1fdaf02a0
Vinicius Maia <maiavini@gmail.com>
2021-03-31 18:56:49 +0100

    Area de Lançamentos
    Local do sistema em que as informações são inseridas

---
+++ Sistema_Notas_Add/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿
+namespace Sistema_Notas_Add
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textMatricula = new System.Windows.Forms.TextBox();
+            this.textNome = new System.Windows.Forms.TextBox();
+            this.comboMateria = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBimestre = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textNota = new System.Windows.Forms.TextBox();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.Matricula = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Materia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nota_1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nota_2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nota_3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nota_4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Media_Final = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Matricula,
+            this.Materia,
+            this.Nome,
+            this.Nota_1,
+            this.Nota_2,
+            this.Nota_3,
+            this.Nota_4,
+            this.Media_Final});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 138);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(853, 168);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textMatricula
+            // 
+            this.textMatricula.Location = new System.Drawing.Point(104, 21);
+            this.textMatricula.Name = "textMatricula";
+            this.textMatricula.Size = new System.Drawing.Size(156, 20);
+            this.textMatricula.TabIndex = 1;
+            // 
+            // textNome
+            // 
+            this.textNome.Location = new System.Drawing.Point(104, 96);
+            this.textNome.Name = "textNome";
+            this.textNome.Size = new System.Drawing.Size(156, 20);
+            this.textNome.TabIndex = 2;
+            // 
+            // comboMateria
+            // 
+            this.comboMateria.FormattingEnabled = true;
+            this.comboMateria.Items.AddRange(new object[] {
+            "Portugês",
+            "Matemática",
+            "História",
+            "Ciências"});
+            this.comboMateria.Location = new System.Drawing.Point(104, 58);
+            this.comboMateria.Name = "comboMateria";
+            this.comboMateria.Size = new System.Drawing.Size(156, 21);
+            this.comboMateria.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Matricula";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Matéria";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Nome";
+            // 
+            // comboBimestre
+            // 
+            this.comboBimestre.FormattingEnabled = true;
+            this.comboBimestre.Items.AddRange(new object[] {
+            "Nota 1",
+            "Nota 2",
+            "Nota 3",
+            "Nota 4",
+            ""});
+            this.comboBimestre.Location = new System.Drawing.Point(375, 22);
+            this.comboBimestre.Name = "comboBimestre";
+            this.comboBimestre.Size = new System.Drawing.Size(156, 21);
+            this.comboBimestre.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(300, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Bimestre";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(300, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Nota";
+            // 
+            // textNota
+            // 
+            this.textNota.Location = new System.Drawing.Point(375, 65);
+            this.textNota.Name = "textNota";
+            this.textNota.Size = new System.Drawing.Size(156, 20);
+            this.textNota.TabIndex = 11;
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(606, 22);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(100, 35);
+            this.btnInserir.TabIndex = 12;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(606, 81);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(100, 35);
+            this.btnExcluir.TabIndex = 13;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(728, 22);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(100, 35);
+            this.btnConectar.TabIndex = 14;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // Matricula
+            // 
+            this.Matricula.HeaderText = "Matricula";
+            this.Matricula.Name = "Matricula";
+            // 
+            // Materia
+            // 
+            this.Materia.HeaderText = "Materia";
+            this.Materia.Name = "Materia";
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            // 
+            // Nota_1
+            // 
+            this.Nota_1.HeaderText = "1º Bimestre";
+            this.Nota_1.Name = "Nota_1";
+            // 
+            // Nota_2
+            // 
+            this.Nota_2.HeaderText = "2º Bimestre";
+            this.Nota_2.Name = "Nota_2";
+            // 
+            // Nota_3
+            // 
+            this.Nota_3.HeaderText = "3º Bimestre";
+            this.Nota_3.Name = "Nota_3";
+            // 
+            // Nota_4
+            // 
+            this.Nota_4.HeaderText = "4º Bimestre";
+            this.Nota_4.Name = "Nota_4";
+            // 
+            // Media_Final
+            // 
+            this.Media_Final.HeaderText = "Média Final";
+            this.Media_Final.Name = "Media_Final";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 450);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.textNota);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBimestre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboMateria);
+            this.Controls.Add(this.textNome);
+            this.Controls.Add(this.textMatricula);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textMatricula;
+        private System.Windows.Forms.TextBox textNome;
+        private System.Windows.Forms.ComboBox comboMateria;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBimestre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textNota;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Matricula;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Materia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nota_1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nota_2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nota_3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nota_4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Media_Final;
+    }
+}
+
---
+++ Sistema_Notas_Add/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlServerCe;
+using System.IO;
+
+namespace Sistema_Notas_Add
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            string baseDados = Application.StartupPath + @"\db\DBSQLServer.sdf";
+            string strConnection = @"DataSource = " + baseDados + "; Password = '1234'";
+
+            SqlCeEngine db = new SqlCeEngine(strConnection);
+
+            SqlCeConnection conexao = new SqlCeConnection(strConnection);
+
+            
+
+            try
+            {
+                conexao.Open();
+
+                SqlCeCommand command = new SqlCeCommand();
+                command.Connection = conexao;
+
+                string nota = textNota.Text;
+                string nome = textNome.Text;
+                string matricula = textMatricula.Text;
+                string materia = comboMateria.Text;
+                string bimestre = comboBimestre.Text;
+
+                command.CommandText = "INSERT INTO alunos " +
+                    "VALUES ("+ nota +","+ nome +","+ matricula +","+ matricula +","+ materia +""+ bimestre + " )";
+                command.ExecuteNonQuery();
+
+                MessageBox.Show("Registro Inserido com Sucesso!" + MessageBoxButtons.OK);
+                command.Dispose();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("ERRO! " + ex );
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            string baseDados = Application.StartupPath + @"\db\DBSQLServer.sdf";
+            string strConnection = @"DataSource = " + baseDados + "; Password = '1234'";
+
+            SqlCeEngine db = new SqlCeEngine(strConnection);
+
+
+            if (!File.Exists(baseDados))
+            {
+                db.CreateDatabase();
+            }
+
+            db.Dispose();
+
+            SqlCeConnection conexao = new SqlCeConnection(strConnection);
+            
+
+            try
+            {
+                conexao.Open();
+
+                SqlCeCommand command = new SqlCeCommand();
+
+                command.Connection = conexao;
+                MessageBox.Show("Banco de Dados Conectado");
+                command.CommandText = "CREATE TABLE alunos (Matricula INT NOT NULL PRIMARY KEY, Materia NVARCHAR(50), Nome NVARCHAR(50))";
+                //command.CommandText = "INSERT INTO alunos VALUES (Nota_1 + Nota_2 + Nota_3 + Nota_4) / 4 WHERE Media_Final ";
+                command.ExecuteNonQuery();
+
+                conexao.Dispose();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("ERRO! " + ex);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+    }
+}
+
